6aeb0b5172c3a21c9c90e43378cef30102e59532
hushengdong <shengdonghu@126.com>
2021-08-13 18:36:19 +0800

    init

---
+++ src/lexical/lexical.go
@@ -0,0 +1,14 @@
+package lexical
+
+import "fmt"
+
+func LexicalAnalysis() {
+
+	//开始词法分析阶段
+	// 1、先读取源程序文本
+	fmt.Println("1、先读取源程序文本")
+	// 2、预处理，去掉空格、注释、换行符等
+	fmt.Println("2、预处理，去掉空格、注释、换行符等")
+	// 3、解析代码
+	fmt.Println("3、解析代码")
+}
